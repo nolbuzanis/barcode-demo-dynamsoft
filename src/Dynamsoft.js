cc2f887a2930a0cebc5cff3a65c0321559d38327
Keillion <735699921@qq.com>
2020-01-10 21:42:19 +0800

    use 7.3 preview version not need custom webpack
    Because we custom the webpack.config.js to let webpack not import the shadow nodejs environment and not build dependence need by node.
    so need react-app-rewired load the additional config-overrides.js.
    dbrjs 7.3 will not need it. we find a new way to define how dbrjs work correctly without config on webpack/rollup.
    Still work both on web and node.

--- src/Dynamsoft.js
+++ src/Dynamsoft.js
@@ -1,5 +1,5 @@
-import Dynamsoft from "dynamsoft-javascript-barcode";
-Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@7.2.3-v2/dist/";
+import Dynamsoft from "keillion-dynamsoft-javascript-barcode";
+Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/keillion-dynamsoft-javascript-barcode@0.20200110.0/dist/";// 0.20200110.0 is preview for dbrjs 7.3.0-v0
 // Please visit https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx to get a trial license
 Dynamsoft.BarcodeReader.productKeys = "t0068NQAAAHFz8G3olEbtsZE7lyhTmeygjYMnxO53Su7Q40dqsDBhVOctKSaRO5tBc1axC5je0Sg3jXZIqSufQKfXoPzzyPc=";
 // Dynamsoft.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.

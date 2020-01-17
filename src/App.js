d01e944bd5adf7d0cb19527ec626691100141a99
Nolan Buzanis <nolan-portl@localhost.lan>
2020-01-17 10:38:09 -0500

    Added custom options

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Dynamsoft from './Dynamsoft';
+import styled from 'styled-components';
 
 const style = {
   video: {
-    width: "640px",
-    height: "480px",
-    border: "1px solid black",
-    resize: "both"
+    position: 'relative',
+    width: "100%",
+    height: "100%",
+    //resize: "both",
+    background: 'black'
   }
 }
+const ScanBox = styled.div`
+  position: absolute;
+  top: 20%;
+  left: 25%;
+  right: 25%;
+  bottom: 60%;
+  border: 2px solid red;
+  height: 20%;
+  width: 50%;
+  z-index: 100;
+`
 
 function App() {
 
@@ -29,6 +42,23 @@ function App() {
       scanner.onUnduplicatedRead = (txt, result) => {
         console.log(result.barcodeFormatString + ': ' + txt);
       };
+      let runtimeSettings = await scanner.getRuntimeSettings();
+      runtimeSettings.barcodeFormatIds = Dynamsoft.EnumBarcodeFormat.BF_ONED;
+      runtimeSettings.localizationModes = [
+        Dynamsoft.EnumLocalizationMode.LM_SCAN_DIRECTLY,
+        Dynamsoft.EnumLocalizationMode.LM_LINES, 0, 0, 0, 0, 0];
+      //runtimeSettings.deblurLevel = 9;
+      runtimeSettings.expectedBarcodesCount = 1;
+      runtimeSettings.region = {
+        regionTop: 20,
+        regionLeft: 25,
+        regionRight: 75,
+        regionBottom: 40,
+        regionMeasuredByPercentage: true
+      }
+      runtimeSettings.returnBarcodeZoneClarity = 1;
+      console.log(runtimeSettings);
+      await scanner.updateRuntimeSettings(runtimeSettings);
       await scanner.open();
 
     } catch (ex) {
@@ -54,7 +84,10 @@ function App() {
       <select className="dbrScanner-sel-camera"></select>
       <select className="dbrScanner-sel-resolution"></select>
       <br />
-      <video className="dbrScanner-video" style={style.video}></video>
+      <div style={{ position: 'relative' }}>
+        <video className="dbrScanner-video" style={style.video}></video>
+        <ScanBox />
+      </div>
     </div>
   );
 }
--- src/Dynamsoft.js
+++ src/Dynamsoft.js
@@ -1,6 +1,9 @@
-import Dynamsoft from "keillion-dynamsoft-javascript-barcode";
+import Dynamsoft, { EnumBarcodeColourMode, EnumLocalizationMode } from "keillion-dynamsoft-javascript-barcode";
 Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/keillion-dynamsoft-javascript-barcode@0.20200110.0/dist/";// 0.20200110.0 is preview for dbrjs 7.3.0-v0
 // Please visit https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx to get a trial license
 Dynamsoft.BarcodeReader.productKeys = "t0068NQAAAHFz8G3olEbtsZE7lyhTmeygjYMnxO53Su7Q40dqsDBhVOctKSaRO5tBc1axC5je0Sg3jXZIqSufQKfXoPzzyPc=";
 // Dynamsoft.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.
+Dynamsoft.EnumBarcodeColourMode = EnumBarcodeColourMode;
+Dynamsoft.EnumLocalizationMode = EnumLocalizationMode;
+//Dynamsoft.BarcodeReader.EnumLocalizationMode = EnumLocalizationMode.LM_LINES;
 export default Dynamsoft;
